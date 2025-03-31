9a3f55949fca7a8e75834f19f0a800b0f1896838
Johannes Löthberg <johannes.loethberg@elokon.com>
2025-03-31 16:15:41 +0200

    WIP: drop unused dependencies
    Not used by the lib
    
    Signed-off-by: Johannes Löthberg <johannes.loethberg@elokon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
-extern crate bimap;
-extern crate clap;
-#[cfg(feature = "derive")]
-extern crate derive;
-extern crate tokio;
-#[cfg(feature = "yaml")]
-extern crate yaml_rust;
-
 #[cfg(feature = "derive")]
 pub use derive::*;
 pub use engine::{Dag, DagError, Engine};
